b1bd81f1091a298e79c0f6652b7f0d4abac6706a
Matthias Cosler <matthias.cosler@cispa.de>
2024-03-13 14:16:25 +0100

    init

---
+++ dataset.py
@@ -0,0 +1,100 @@
+""" SAT dataset"""
+
+import os
+from typing import Generator, Self
+import pandas as pd
+
+from datatypes import SATSample
+
+from collections.abc import MutableMapping
+
+
+class SATDataset:
+    """Dataset of SAT Problems"""
+
+    def __init__(self, df: pd.DataFrame) -> None:
+        self.df = df
+
+    @classmethod
+    def load(cls, load_from: str) -> Self:
+        """Load the dataset from a csv file"""
+        return cls(
+            pd.read_csv(
+                load_from, converters={"formula": lambda x: str(x).replace("\\n", "\n")}
+            )
+        )
+
+    def sample(self, seed: int | None = None, **kwargs) -> None:
+        """Return a random sample of items from an axis of object.
+        Wrapper for pandas.DataFrame.sample
+        """
+        self.df = self.df.sample(random_state=seed, **kwargs)
+
+    def shuffle(self, seed: int | None = None, **kwargs) -> None:
+        """Shuffle the dataset"""
+        self.df = self.df.sample(frac=1, random_state=seed, **kwargs).reset_index(
+            drop=True
+        )
+
+    def generator(self) -> Generator[SATSample, None, None]:
+        """Create a generator for all samples in the dataset."""
+        for _, row in self.df.iterrows():
+            row = row.dropna()
+            yield SATSample.from_fields(**row.to_dict())
+
+
+class SplitDataset(MutableMapping):
+    """Combines multiple splits"""
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.store = {}
+        self.update(dict(*args, **kwargs))
+
+    @property
+    def splits(self) -> dict[str, SATDataset]:
+        return self.store
+
+    @property
+    def split_names(self) -> list[str]:
+        return list(self)
+
+    def __getitem__(self, key: str) -> SATDataset:
+        return self.store[key]
+
+    def __setitem__(self, key: str, value: SATDataset) -> None:
+        self.store[key] = value
+
+    def __delitem__(self, key):
+        del self.store[key]
+
+    def __len__(self) -> int:
+        return len(self.store)
+
+    def __iter__(self):
+        return iter(self.store)
+
+    def generator(
+        self, splits: list[str] | None = None, **kwargs
+    ) -> Generator[SATSample, None, None]:
+        """Yields dataset samples"""
+        split_names = splits if splits else self.split_names
+        for name in split_names:
+            split = self[name]
+            for sample in split.generator(**kwargs):
+                yield sample
+
+    @classmethod
+    def load(cls, path: str):
+        path = os.path.expanduser(path)
+        return cls(
+            **{
+                p.name: SATDataset.load(os.path.join(p.path, p.name + ".csv"))
+                for p in os.scandir(path)
+                if p.is_dir()
+            },
+            **{
+                p.name[:-4]: SATDataset.load(p.path)
+                for p in os.scandir(path)
+                if p.name.endswith(".csv")
+            },
+        )
---
+++ datatypes.py
@@ -0,0 +1,230 @@
+""" SAT datatypes"""
+
+from abc import ABCMeta, abstractmethod
+from typing import Iterator, Self
+
+
+class ParsingException(Exception):
+    pass
+
+
+class CSVDType(metaclass=ABCMeta):
+    """Abstract Class for all DTypes that can be created from a CSV row"""
+
+    @classmethod
+    @abstractmethod
+    def from_fields(cls, **kwargs) -> Self:
+        """Create this DType from a dict based on the CSV file"""
+
+    @abstractmethod
+    def to_fields(self) -> dict[str, str]:
+        """Create a dict representing this DType for csv writing"""
+
+
+class Literals(metaclass=ABCMeta):
+    """Abstract class that summarizes a collection of literals."""
+
+    def __init__(self, lits: list[int]) -> None:
+        if len(set(abs(lit) for lit in lits)) != len(lits):
+            raise ParsingException("Literals not distinct or a tautology")
+
+        if 0 in lits:
+            raise ParsingException("0 cannot be a literal")
+
+        self.lits: list[int] = lits
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({str(self.lits)})"
+
+    @classmethod
+    def from_strs(cls, lit_strs: list[str]) -> Self:
+        """create from a list of string literals
+
+        Args:
+            lit_strs (list[str]): the list of string literals
+
+        Raises:
+            ParsingException: if not all literals are ints
+        """
+        try:
+            lits = [int(lit_str) for lit_str in lit_strs]
+        except ValueError as exc:
+            raise ParsingException("Non-integer in literal list") from exc
+        return cls(lits)
+
+    @property
+    def max_var(self) -> int:
+        """The largest literal in this set"""
+        return abs(max(self.lits, key=abs))
+
+    def __len__(self) -> int:
+        """the number of literals"""
+        return len(self.lits)
+
+    def __iter__(self) -> Iterator[int]:
+        return iter(self.lits)
+
+    def sort(self) -> None:
+        """sort the literals according the abstract value"""
+        self.lits.sort(key=abs)
+
+    def polarity(self, literal: int) -> bool:
+        """Get the polarity of a literal
+
+        Raises:
+            ValueError: if the literal does not exist in the set
+        """
+        if literal in [abs(lit) for lit in self.lits if lit < 0]:
+            return False
+        if literal in [abs(lit) for lit in self.lits if lit > 0]:
+            return True
+        raise ValueError("Literal not defined")
+
+
+class Clause(Literals):
+    """A clause (set of literals)"""
+
+    def to_str(self) -> str:
+        """return the clause in the DIMACS CNF Format"""
+        return f"{" ".join([str(lit) for lit in self.lits])}{' ' if self.lits else ''}0"
+
+    def __str__(self) -> str:
+        return self.to_str()
+
+    @classmethod
+    def from_str(cls, from_str: str) -> Self:
+        """create a clause from a clause in the DIMACS CNF Format"""
+        return cls.from_strs(from_str.rstrip("0").rstrip(" ").split(" "))
+
+
+class SATAssignment(Literals, CSVDType):
+
+    @classmethod
+    def from_fields(cls, assignment: str, **_) -> Self:
+        return cls.from_str(from_str=assignment)
+
+    def to_fields(self) -> dict[str, str]:
+        return {"assignment": self.to_str()}
+
+    def to_str(self) -> str:
+        """return the assignment as comma-separated string of literals"""
+        return ",".join([str(lit) for lit in self.lits])
+
+    def __str__(self) -> str:
+        return self.to_str()
+
+    @classmethod
+    def from_str(cls, from_str: str) -> Self:
+        """create an assignment from a comma separated string of literals"""
+        return cls.from_strs(from_str.split(","))
+
+
+class CNFFormula(CSVDType):
+    def __init__(
+        self, clauses: list[Clause], comments: list[str] | None = None
+    ) -> None:
+        self.clauses: list[Clause] = clauses
+        self._num_vars = None
+        self.comments = comments if comments is not None else []
+
+    @classmethod
+    def from_fields(cls, formula: str, **_) -> Self:
+        return cls.from_str(formula)
+
+    def to_fields(self) -> dict[str, str]:
+        return {"formula": self.to_str()}
+
+    def __iter__(self) -> Iterator[Clause]:
+        return iter(self.clauses)
+
+    @property
+    def nbclauses(self) -> int:
+        """the number of clauses"""
+        return len(self.clauses)
+
+    @property
+    def nbvars(self) -> int:
+        "the number of literals"
+        return (
+            self._num_vars if self._num_vars else max(c.max_var for c in self.clauses)
+        )
+
+    @classmethod
+    def from_str(cls, from_str: str) -> Self:
+        """Create CNFFormula from a string in DIMACS CNF format"""
+        nbvars = -1
+        nbclauses = -1
+        clauses: list[Clause] = []
+        comments: list[str] = []
+        from_str = from_str.rstrip("\n")
+        for clause_str in from_str.split("\n"):
+            if clause_str.startswith("c"):
+                comments.append(clause_str[2:])
+            elif clause_str.startswith("p"):
+                header = clause_str.split(" ")
+                if nbvars != -1 or nbclauses != -1 or header[1] != "cnf":
+                    raise ParsingException("could not read header")
+                nbvars = int(header[2])
+                nbclauses = int(header[3])
+            else:
+                clauses.append(Clause.from_str(clause_str))
+
+        formula = cls(clauses)
+
+        if nbclauses == -1 or nbvars == -1:
+            raise ParsingException("No header found")
+
+        if nbclauses != formula.nbclauses:
+            raise ParsingException(
+                "Specified number of clauses does not match parsed number of clauses"
+            )
+
+        if nbvars != formula.nbvars:
+            raise ParsingException(
+                "Specified number of variables does not match parsed number of variables"
+            )
+
+        return formula
+
+    def to_str(self) -> str:
+        """Create the DIMACS CNF format representation of this CNF"""
+        comment_str = "".join([f"c {co}\n" for co in self.comments])
+        header_str = f"p cnf {self.nbvars} {self.nbclauses}\n"
+        clause_str = "".join([cl.to_str() + "\n" for cl in self.clauses])
+        return comment_str + header_str + clause_str
+
+    def __str__(self) -> str:
+        return self.to_str()
+
+    def __repr__(self) -> str:
+        clauses = f"[{", ".join([c.__repr__() for c in self.clauses])}]"
+        return f"{self.__class__.__name__}({clauses},{str(self.comments)})"
+
+
+class SATSample(CSVDType):
+    """combination of a SAT Problem (i.e. CNF formula) and SAT solution (i.e. assignment)"""
+
+    def __init__(self, formula: CNFFormula, assignment: SATAssignment) -> None:
+        self.formula = formula
+        self.assignment = assignment
+
+    @property
+    def input(self) -> CNFFormula:
+        return self.formula
+
+    @property
+    def target(self) -> SATAssignment:
+        return self.assignment
+
+    @classmethod
+    def from_fields(cls, **kwargs) -> Self:
+        return cls(
+            formula=CNFFormula.from_fields(**kwargs),
+            assignment=SATAssignment.from_fields(**kwargs),
+        )
+
+    def to_fields(self) -> dict[str, str]:
+        return {**self.formula.to_fields(), **self.assignment.to_fields()}
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.formula.__repr__(), self.assignment.__repr__()})"
